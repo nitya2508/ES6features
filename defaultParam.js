7299fdecfd7993492bfbd0c28f799ba4ae64b1ab
nitya2508 <nityashree2508@gmail.com>
2021-12-24 12:44:12 +0530

    added 5 files

---
+++ defaultParam.js
@@ -0,0 +1,13 @@
+var calculateArea = function(height = 50, width = 80) {  
+   area=height * width;
+   return area;
+}
+
+var calcArea = calculateArea();
+console.log(calcArea);
+
+console.log(calculateArea(20));
+
+console.log(calculateArea(20, 30));
+
+console.log(calculateArea(0, 20));
---
+++ templateLiterals.js
@@ -0,0 +1,5 @@
+var firstName = "Nitya";
+var lastName = "Shree";
+
+var name = `My name is ${firstName} ${lastName}.`
+console.log(name)

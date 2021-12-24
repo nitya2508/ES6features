7870e483cd0695b41be990ea45e8731d8d2a8ca9
nitya2508 <nityashree2508@gmail.com>
2021-12-24 12:30:56 +0530

    added

---
+++ mulyiline.js
@@ -0,0 +1,13 @@
+let poemData = `Johny Johny Yes Papa,     
+                Eating sugar?  No, papa!
+                Telling lies? No, papa!
+                Open your mouth Ah, ah, ah!`
+
+    console.log(poemData); 
+
+    var poem = 'Johny Johny Yes Papa,\n'     
+             + 'Eating sugar?  No, papa!\n'
+             + 'Telling lies? No, papa!\n'
+             + 'Open your mouth Ah, ah, ah!'
+
+    console.log(poem)
